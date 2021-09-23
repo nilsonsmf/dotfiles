0f0563ebbd03fd1bff5940d606adca8ce053574f
nilsonsmf <51447494+nilsonsmf@users.noreply.github.com>
2021-09-23 16:14:26 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ ln -s -f $DIR/bashrc $HOME/.bashrc
 ln -s -f $DIR/scripts/env $HOME/.env
 ln -s -f $DIR/gitconfig $HOME/.gitconfig
 ln -s -f $DIR/gitignore_global $HOME/.gitignore_global
-#ln -s -f $DIR/i3 $HOME/.i3
 #test -d "$HOME/.ssh" || mkdir "$HOME/.ssh"
 #ln -s -f $DIR/ssh_config $HOME/.ssh/config
 #ln -s -f $DIR/terminator_config $HOME/.config/terminator/config
